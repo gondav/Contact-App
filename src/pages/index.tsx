ced62a12f068ca08dc40e7884198e7a32a159f48
gondav <viktoria.gonda@gmail.com>
2023-10-01 14:42:19 +0200

    add Header component to index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import { api } from "~/utils/api";
+import Header from "./components/Header";
 
 export default function Home() {
   const contacts = api.contact.getAll.useQuery();
 
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
