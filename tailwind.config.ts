eb42ef1a4e6bab56f3f891a8f5d6d6b197b5e9d7
gondav <viktoria.gonda@gmail.com>
2023-10-01 11:48:08 +0200

    extend tailwind color system with customGrey

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,22 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customGrey: {
+          10: "#414141",
+          20: "#3C3C3C ",
+          30: "#373737",
+          40: "#323232",
+          50: "#2D2D2D",
+          60: "#282828",
+          70: "#232323",
+          80: "#1E1E1E",
+          90: "#191919",
+          100: "#141414",
+        },
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
