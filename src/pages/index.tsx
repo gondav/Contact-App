362890b60d7165b7dc7883e06478263ae24dc2ee
gondav <viktoria.gonda@gmail.com>
2023-10-01 16:56:22 +0200

    add main with ContactListItem component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import { api } from "~/utils/api";
 import Header from "./components/Header";
+import ContactListItem from "./components/ContactListItem";
 
 export default function Home() {
   const contacts = api.contact.getAll.useQuery();
+  const contactList = contacts.data?.data.contacts;
 
   return (
     <>
       <Header />
+      <main className="h-full ">
+        <div className="mx-auto h-full border-customGrey-60 md:w-[48rem] md:border-x">
+          <div className="contact-list-container mx-6 pt-3">
+            {contactList?.map((contact, i) => (
+              <ContactListItem key={i} contact={contact} />
+            ))}
+          </div>
+        </div>
+      </main>
     </>
   );
 }
