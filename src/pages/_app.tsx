db11286203ae43de760d7c2d30e651c12af08349
gondav <viktoria.gonda@gmail.com>
2023-10-01 09:36:48 +0200

    move Head tag to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import { type AppType } from "next/app";
-
 import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Contact App</title>
+        <meta name="description" content="This is a contact application" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
