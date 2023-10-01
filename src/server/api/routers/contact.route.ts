833ea87cfe70f55a5ed3c2466554796bee8c5bf3
gondav <viktoria.gonda@gmail.com>
2023-10-01 20:13:11 +0200

    add delete method to contactRouter

--- src/server/api/routers/contact.route.ts
+++ src/server/api/routers/contact.route.ts
@@ -1,11 +1,18 @@
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { getContactsHandler } from "../controllers/contact.controller";
+import {
+  deleteContactHandler,
+  getContactsHandler,
+} from "../controllers/contact.controller";
+import { params } from "../schemas/contact.schema";
 
 const contactRouter = createTRPCRouter({
   getAll: publicProcedure.query(async () => {
     const response = await getContactsHandler();
     return response;
   }),
+  delete: publicProcedure
+    .input(params)
+    .mutation(({ input }) => deleteContactHandler({ paramsInput: input })),
 });
 
 export default contactRouter;
