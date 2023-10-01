94d8a8c6d86188fe509bafb075d23bfe205954b2
gondav <viktoria.gonda@gmail.com>
2023-10-01 19:07:26 +0200

    add Modal component to ContactListItem component

--- src/pages/components/ContactListItem.tsx
+++ src/pages/components/ContactListItem.tsx
@@ -1,6 +1,8 @@
 import muteSrc from "../../icons/mute.png";
 import callSrc from "../../icons/call.png";
 import moreSrc from "../../icons/more.png";
+import Modal from "./Modal";
+import { useState } from "react";
 
 interface Contact {
   id: number;
@@ -19,8 +21,14 @@ const ContactListItem = ({ contact }: ContactListItemProps) => {
   const defaultImageSrc =
     "https://ux-contact-profile-pictures.s3.eu-north-1.amazonaws.com/Default.png";
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
-    <div className="contact-container group flex justify-between">
+    <div className="contact-container group relative  flex justify-between">
       <div className="contact-left flex items-center py-3 ">
         <img
           className="mr-4 h-10 w-10 rounded-full border border-customGrey-60 object-cover"
@@ -32,11 +40,19 @@ const ContactListItem = ({ contact }: ContactListItemProps) => {
           <p className="message">{phoneNumber ?? ""}</p>
         </div>
       </div>
-      <div className="contact-right flex items-center gap-x-2 opacity-0 transition-opacity duration-500 group-hover:opacity-100">
+      <div className="contact-right  flex items-center gap-x-2 opacity-0 transition-opacity duration-500 group-hover:opacity-100">
         <img className="h-5 w-5" src={muteSrc.src} alt="Mute" />
+
         <img className="h-5 w-5" src={callSrc.src} alt="Call" />
-        <img className="h-5 w-5" src={moreSrc.src} alt="More" />
+
+        <img
+          className="h-5 w-5 cursor-pointer"
+          src={moreSrc.src}
+          alt="More"
+          onClick={toggleModal}
+        />
       </div>
+      <Modal isOpen={isModalOpen} />
     </div>
   );
 };
