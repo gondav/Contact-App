d8c9c9b8a151710558494ad9da8940c56ab06272
gondav <viktoria.gonda@gmail.com>
2023-10-01 16:57:17 +0200

    minor adjustment in header style

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -7,18 +7,18 @@ import profilePicSrc from "../../icons/profile-pic.png";
 const Header = () => {
   return (
     <header>
-      <div className="border-customGrey-60 h-14 border-b md:h-24 ">
-        <div className="border-customGrey-60 mx-auto h-full max-w-3xl md:border-x"></div>
+      <div className="h-14 border-b border-customGrey-60 md:h-24 ">
+        <div className="mx-auto h-full max-w-3xl border-customGrey-60 md:border-x"></div>
       </div>
-      <div className="border-customGrey-60 h-24 border-b">
+      <div className="h-24 border-b border-customGrey-60">
         <div className="flex h-24 items-start justify-center md:items-center">
           <img
             className="mx-4 mt-3 md:mx-6 md:mt-0"
             src={backArrowSrc.src}
             alt="Back arrow"
           />
-          <div className="border-customGrey-60 h-full w-[48rem] md:border-x  md:p-6">
-            <nav className="flex h-full flex-col items-center justify-around md:flex-row">
+          <div className="h-full w-[48rem] border-customGrey-60 md:border-x  md:p-6">
+            <nav className="flex h-full flex-col items-center justify-between md:flex-row">
               <h1>Contacts</h1>
               <div className="flex h-full items-center">
                 <img className="mr-4" src={settingsSrc.src} alt="Settings" />
@@ -27,7 +27,7 @@ const Header = () => {
                   src={profilePicSrc.src}
                   alt="Profile picture"
                 />
-                <button className="bg-customGrey-60 flex items-center rounded-3xl px-3 py-2">
+                <button className="flex items-center rounded-3xl bg-customGrey-60 px-3 py-2">
                   <img className=" md:mr-3" src={addSrc.src} alt="Add" />
                   <span className="hidden text-white md:block">Add new</span>
                 </button>
