2e4b08f0aeedae504581092a1bc28164810cd630
gondav <viktoria.gonda@gmail.com>
2023-10-02 19:15:11 +0200

    add createContactSchema function

--- src/server/api/schemas/contact.schema.ts
+++ src/server/api/schemas/contact.schema.ts
@@ -4,4 +4,14 @@ export const params = object({
   contactId: string(),
 });
 
+export const createContactSchema = object({
+  name: string({
+    required_error: "Name is required",
+  }),
+  phoneNumber: string(),
+  email: string(),
+  imageUrl: string(),
+});
+
 export type ParamsInput = TypeOf<typeof params>;
+export type CreateContactInput = TypeOf<typeof createContactSchema>;
