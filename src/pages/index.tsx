d5014b7318b959f85954830627e2d40eec7f39a8
gondav <viktoria.gonda@gmail.com>
2023-10-02 17:56:18 +0200

    remove contact feature implemented

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,12 @@ import Header from "./components/Header";
 import ContactListItem from "./components/ContactListItem";
 
 export default function Home() {
-  const contacts = api.contact.getAll.useQuery();
-  const contactList = contacts.data?.data.contacts;
+  const { data, refetch } = api.contact.getAll.useQuery();
+  const contacts = data?.data.contacts;
+
+  const handleContactRemove = () => {
+    refetch();
+  };
 
   return (
     <>
@@ -12,8 +16,12 @@ export default function Home() {
       <main className="h-full bg-customGrey-100">
         <div className="mx-auto h-full border-customGrey-60 md:w-[48rem] md:border-x">
           <div className="contact-list-container mx-6 pt-3">
-            {contactList?.map((contact, i) => (
-              <ContactListItem key={i} contact={contact} />
+            {contacts?.map((contact, i) => (
+              <ContactListItem
+                key={i}
+                contact={contact}
+                onRemove={handleContactRemove}
+              />
             ))}
           </div>
         </div>
