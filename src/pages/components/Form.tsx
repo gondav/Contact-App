08b9b94268a0be290ae6c192935267336bce5eb0
gondav <viktoria.gonda@gmail.com>
2023-10-03 23:19:47 +0200

    add a form overlay

--- src/pages/components/Form.tsx
+++ src/pages/components/Form.tsx
@@ -69,94 +69,96 @@ const Form = ({ formatType, contactToUpdate, onClose }: FormProps) => {
   };
 
   return (
-    <div className="rounded-lg bg-customGrey-90  p-6 sm:w-[364px]">
-      <h2 className="mb-6">{heading}</h2>
-      <form>
-        <div className="form-wrapper ">
-          <div className="picture-field-wrapper mb-6 flex  items-center">
-            <img
-              src={DEFAULT_IMG}
-              alt="Contact picture"
-              className="mr-4 w-[88px] rounded-full border-customGrey-70"
-            />
-            <DefaultButton iconSrc={addSrc.src} label="Add picture" />
-            <div className="ml-2 hidden">
-              <DefaultButton iconSrc={deleteSrc.src} />
+    <div className="rounded-lg bg-customGrey-100   sm:w-[364px]">
+      <div className="form-overlay p-6">
+        <h2 className="mb-6">{heading}</h2>
+        <form>
+          <div className="form-wrapper ">
+            <div className="picture-field-wrapper mb-6 flex  items-center">
+              <img
+                src={DEFAULT_IMG}
+                alt="Contact picture"
+                className="mr-4 w-[88px] rounded-full border-customGrey-70"
+              />
+              <DefaultButton iconSrc={addSrc.src} label="Add picture" />
+              <div className="ml-2 hidden">
+                <DefaultButton iconSrc={deleteSrc.src} />
+              </div>
+            </div>
+            <div className="mb-6 flex flex-col">
+              <label
+                htmlFor="name"
+                className="message mb-1 text-white opacity-[.56] "
+              >
+                Name
+              </label>
+              <input
+                ref={nameRef}
+                className={`input-field ${
+                  contact.name === ""
+                    ? "placeholder-opacity-[0.32]"
+                    : "placeholder-white"
+                }`}
+                type="text"
+                id="name"
+                placeholder={contact.name || "Jamie Wright"}
+              />
+            </div>
+            <div className="mb-6 flex flex-col">
+              <label
+                htmlFor="phoneNumber"
+                className="message mb-1 text-white opacity-[.56]"
+              >
+                Phone number
+              </label>
+              <input
+                ref={phoneRef}
+                className={`input-field ${
+                  contact.phoneNumber === ""
+                    ? "placeholder-opacity-[0.32]"
+                    : "placeholder-white"
+                }`}
+                type="text"
+                id="phoneNumber"
+                placeholder={contact.phoneNumber || "+01 234 5678"}
+              />
+            </div>
+            <div className="mb-6 flex flex-col">
+              <label
+                htmlFor="email"
+                className="message mb-1 text-white opacity-[.56]"
+              >
+                Email address
+              </label>
+              <input
+                ref={emailRef}
+                className={`input-field ${
+                  contact.email === ""
+                    ? "placeholder-opacity-[0.32]"
+                    : "placeholder-white"
+                }`}
+                type="text"
+                id="email"
+                placeholder={contact.email || "jamie.wright@mail.com"}
+              />
             </div>
           </div>
-          <div className="mb-6 flex flex-col">
-            <label
-              htmlFor="name"
-              className="message mb-1 text-white opacity-[.56] "
-            >
-              Name
-            </label>
-            <input
-              ref={nameRef}
-              className={`input-field ${
-                contact.name === ""
-                  ? "placeholder-opacity-[0.32]"
-                  : "placeholder-white"
-              }`}
-              type="text"
-              id="name"
-              placeholder={contact.name || "Jamie Wright"}
+          <div className="btn-container flex items-center justify-end space-x-2 pt-6">
+            <DefaultButton
+              label="Cancel"
+              className="btn-secondary"
+              onClick={onClose}
+            />
+            <DefaultButton
+              label="Done"
+              onClick={() => {
+                createContactObject();
+                onClose();
+              }}
             />
           </div>
-          <div className="mb-6 flex flex-col">
-            <label
-              htmlFor="phoneNumber"
-              className="message mb-1 text-white opacity-[.56]"
-            >
-              Phone number
-            </label>
-            <input
-              ref={phoneRef}
-              className={`input-field ${
-                contact.phoneNumber === ""
-                  ? "placeholder-opacity-[0.32]"
-                  : "placeholder-white"
-              }`}
-              type="text"
-              id="phoneNumber"
-              placeholder={contact.phoneNumber || "+01 234 5678"}
-            />
-          </div>
-          <div className="mb-6 flex flex-col">
-            <label
-              htmlFor="email"
-              className="message mb-1 text-white opacity-[.56]"
-            >
-              Email address
-            </label>
-            <input
-              ref={emailRef}
-              className={`input-field ${
-                contact.email === ""
-                  ? "placeholder-opacity-[0.32]"
-                  : "placeholder-white"
-              }`}
-              type="text"
-              id="email"
-              placeholder={contact.email || "jamie.wright@mail.com"}
-            />
-          </div>
-        </div>
-        <div className="btn-container flex items-center justify-end space-x-2 pt-6">
-          <DefaultButton
-            label="Cancel"
-            className="btn-secondary"
-            onClick={onClose}
-          />
-          <DefaultButton
-            label="Done"
-            onClick={() => {
-              createContactObject();
-              onClose();
-            }}
-          />
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 };
